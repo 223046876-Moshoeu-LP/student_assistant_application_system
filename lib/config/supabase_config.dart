e3b0acebdf9a7fe8c2c0947631e7291035d8cc47
Jobe-designer <asandasithole0312@gmail.com>
2026-05-19 19:37:46 +0200

    22300460 changed the content of the file lib/config/supabase_config.dart

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,4 +1,8 @@
-// lib/config/supabase_config.dart
+/* 
+Student Numbers: 223046876, 223000460, 223050336, 223040081, 224000274, 224027806
+Student Names: Lehlogonolo Moshoeu, Asanda Sithole, Sandile Pheko, Mvelo Masinga, Mponisi Nkuna, Cedric Motone
+Questions: SupabaseConfig class centralizes all Supabase-related configuration, including project URL, anon key, bucket names, and admin secret code. This allows for easy management of these values in one place and keeps them separate from the main application logic.
+*/ 
 
 class SupabaseConfig {
   static const String url = 'https://xxjzmzkqsbpasbnepzdc.supabase.co';
